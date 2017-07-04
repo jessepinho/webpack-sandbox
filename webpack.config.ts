a32bc468341999595874108e589859ec7dbac402
Jesse Pinho <jesse@jessepinho.com>
2017-07-04 22:16:55 +0200

    Add StatsPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as StatsPlugin from 'stats-webpack-plugin';
 import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
@@ -6,8 +7,17 @@ const config: webpack.Configuration = {
     main: path.resolve(__dirname, './index.js'),
     main2: path.resolve(__dirname, './index.js'),
   },
+
   output: {
     filename: './index.[name].compiled.js',
+  },
+
+  plugins: [
+    new StatsPlugin('stats.json'),
+  ],
+
+  stats: {
+    timings: true,
   },
 
   // This has no effect:
